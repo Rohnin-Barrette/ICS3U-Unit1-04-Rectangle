3d5f6609d92d3eff029e9ca341f4d0d291bea999
Rohnin Barrete <rohnin.barrette@mths.ca>
2021-09-11 17:25:56 +0000

    Made a math program that finds the area and perimiter of a rectangle.
    Signed-off-by: Rohnin Barrete <rohnin.barrette@mths.ca>

---
+++ Rectangle.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 Rohnin Barrette rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2021
+// This program calculates the area and perimeter of a rectangle
+//    with dimensions 5cm x 3cm
+
+#include <iostream>
+// Define a namespace instead of typing std:: all the time
+using namespace std;
+
+int main() {
+    int length = 5;
+    int width = 3;
+    
+    cout << "calculate the area of the following: " << endl;
+    cout << "Area = length cm * width cm" << endl;
+    cout << "Area = " << length << "cm * " << width << "cm" << " = " << (length * width) << "cm^2\n" <<endl;
+    
+    cout << "calculate the perimiter of the following:" <<endl;
+    cout << "perimeter = (length + width)*2 " << endl;
+    cout << "perimeter = (" << length << "cm + " << width << "cm )* 2 = " << ((length + width) *2 ) << endl;
+
+// Return 0 at the end of a function  that is supposed to return an int.
+    return 0;
+}
