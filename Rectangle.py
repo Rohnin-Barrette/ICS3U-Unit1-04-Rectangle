8cbb97696c780b21f6a0331e6976a871acaeecfa
Rohnin Barrete <rohnin.barrette@mths.ca>
2021-09-13 20:59:50 +0000

    redid the area and perimeter program

--- Rectangle.py
+++ Rectangle.py
@@ -2,21 +2,20 @@
 
 # Created by: Rohnin Barrette
 # Created on: Sept 2021
-# This is the "Hello, World!" Program, with proper style
+# This is a program that calculates the area and perimiter of a rectangle.
 
 
 def main():
+    # This is a program that calculates the area and perimiter of a rectangle.
     length = 5
     width = 3
     print("calculate the area and the perimiter of the following")
     print("area = length * width ")
-    print(
-        "area of  {}cm * {}cm = {}cm^2  ".format(length, width, length * width)
-    )
+    print("area of {}cm * {}cm = {} cm²".format(length, width, length * width))
     print("\ncalculate the perimiter of the following")
     print("perimiter = (length + width) *2")
     print(
-        "perimiter of  {}cm + {}cm = {}cm   ".format(
+        "perimiter of  {}cm + {}cm = {} cm   ".format(
             length, width, (length + width) * 2
         )
     )
