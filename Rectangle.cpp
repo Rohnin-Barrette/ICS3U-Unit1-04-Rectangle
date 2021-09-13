56ba01d7a8d6730edc00467d205f6ec41b3cdfa1
Rohnin Barrete <rohnin.barrette@mths.ca>
2021-09-13 21:17:10 +0000

    Redid the rectangle program in cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -10,17 +10,20 @@
 using namespace std;
 
 int main() {
+    // variables for length and width
     int length = 5;
     int width = 3;
-    
+
     cout << "calculate the area of the following: " << endl;
     cout << "Area = length cm * width cm" << endl;
-    cout << "Area = " << length << "cm * " << width << "cm" << " = " << (length * width) << "cm^2\n" <<endl;
-    
+    cout << "Area = " << length << "cm * " << width << "cm" << " = "
+        << (length * width) << "cm²\n" <<endl;
+
     cout << "calculate the perimiter of the following:" <<endl;
     cout << "perimeter = (length + width)*2 " << endl;
-    cout << "perimeter = (" << length << "cm + " << width << "cm )* 2 = " << ((length + width) *2 ) << endl;
+    cout << "perimeter = (" << length << "cm + " << width << "cm )* 2 = "
+        << ((length + width) *2) <<"cm" << endl;
 
-// Return 0 at the end of a function  that is supposed to return an int.
+//  Return 0 at the end of a function that is supposed to return an int.
     return 0;
 }
